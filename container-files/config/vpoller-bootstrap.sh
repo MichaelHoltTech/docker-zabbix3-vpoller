d2bcef21636866bc8a21e94b6c122d5353d5fa6b
Michael Holt <michael.holt@ladb.com>
2016-03-31 12:40:26 -0700

    Initiate vPoller Database

--- container-files/config/vpoller-bootstrap.sh
+++ container-files/config/vpoller-bootstrap.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-set -e
-set -u
-
 cp /usr/local/src/vpoller-module/vpoller.so /usr/lib/zabbix/modules
 
 /config/bootstrap.sh
+
+if [ ! -f /var/lib/vconnector/vconnector.db ]; then
+  vconnector-cli init
+  vconnector-cli -H ${VS_HOST} -U ${VS_UN} -P ${VS_PW} add
+  vconnector-cli -H ${VS_HOST} enable
+  supervisorctl restart vpoller-worker
+fi
