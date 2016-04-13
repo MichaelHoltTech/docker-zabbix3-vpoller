c4eb8e3ac4431d738f11443bad1b789ef0f20895
Michael Holt <michael@mholt.tech>
2016-04-13 15:37:46 -0700

    Initial Converstion to Pusion/Baseimage

--- Dockerfile
+++ Dockerfile
@@ -1,59 +1,63 @@
-FROM zabbix/zabbix-3.0:3.0.1
+# Use phusion/baseimage as base image. To make your builds reproducible, make
+# sure you lock down to a specific version, not to `latest`!
+# See https://github.com/phusion/baseimage-docker/blob/master/Changelog.md for
+# a list of version numbers.
+FROM phusion/baseimage:0.9.15
 
-COPY container-files /
+# Use baseimage-docker's init system.
+CMD ["/sbin/my_init"]
 
-# Layer: py-vpoller
+# ...put your own build instructions here...
+RUN apt-get update && apt-get upgrade -y -o Dpkg::Options::="--force-confold" && apt-get dist-upgrade -y
+
+ADD container-files-base /
+
+COPY container-files-zabbix /
+
+# Get Zabbix built from Source
 RUN \
-    chmod +x /config/vpoller-bootstrap.sh /usr/local/bin/zabbix-vsphere-import /usr/local/bin/vsphere-import && \
-    yum -y update && \
-    yum install -y nano python34 gcc gcc-c++ glib* automake autoconf libtool python34-devel git psmisc && \
-  	yum groupinstall -y "development tools" && \
-    yum clean all && \
-    curl https://bootstrap.pypa.io/get-pip.py | python3.4 && \
-    pip3 install vpoller pyyaml pyzabbix && \
-    mkdir -p /var/run/vpoller /var/log/vpoller /var/lib/vconnector /var/log/vconnector && \
-    libzmq_dir=$( mktemp -d /tmp/libzmq.XXXXXX ) && \
-    git clone https://github.com/zeromq/zeromq4-x.git ${libzmq_dir} && \
-    cd ${libzmq_dir} && \
-    ./autogen.sh && \
-    ./configure && \
-    make && make install && make clean && \
-    ldconfig && \
-    git clone --no-checkout https://github.com/dnaeon/py-vpoller /usr/local/src/vpoller-cclient && \
-    cd /usr/local/src/vpoller-cclient && \
-    git config core.sparseCheckout true && \
-    echo "extra/vpoller-cclient*"> .git/info/sparse-checkout && \
-    git checkout master && \
-    mv extra/vpoller-cclient/* . && \
-    rm -Rf extra && \
-    cd /usr/local/src/vpoller-cclient && \
-    make && \
-    mv vpoller-cclient /usr/local/bin && \
-    chmod +x /usr/local/bin/vpoller-cclient
+  sudo add-apt-repository ppa:openjdk-r/ppa && \
+  sudo apt-get update && \
+  apt-get dist-upgrade -y && \
+  apt-get install \
+          wget subversion nginx inotify-tools php5-fpm \
+          php5-gd php5-cli php5-mysqlnd php5-snmp php5-ldap \
+          gcc build-essential nano pkg-config fping libaio1 \
+          libdbd-mysql-perl libdbi-perl libhtml-template-perl \
+          libiksemel3 libltdl7 libmysqlclient18 libodbc1 \
+          libopenipmi0 libssh2-1 snmpd mysql-client \
+          mysql-client-5.5 mysql-client-core-5 mysql-common \
+          git-core libxml2-dev libxslt-dev mysql-client \
+          libmysqlclient-dev python-dev python-setuptools \
+          swig python-imaging python-m2crypto gettext \
+          python-iso8601 python3.4-dev libiksemel-dev \
+          unixodbc-dev unixodbc-bin unixodbc libsnmp-dev \
+          libssh2-1-dev libopenipmi-dev libcurl4-openssl-dev \
+          libmysqld-dev openjdk-8-jdk ruby python-pip python3-pip && \
+  svn co svn://svn.zabbix.com/tags/3.0.1 /usr/local/src/zabbix && \
+  cd /usr/local/src/zabbix && \
+  DATE=`date +%Y-%m-%d` && \
+  sed -i "s/ZABBIX_VERSION.*'\(.*\)'/ZABBIX_VERSION', '\1 ($DATE)'/g" frontends/php/include/defines.inc.php && \
+  sed -i "s/ZABBIX_VERSION_RC.*\"\(.*\)\"/ZABBIX_VERSION_RC \"\1 (${DATE})\"/g" include/version.h && \
+  sed -i "s/String VERSION =.*\"\(.*\)\"/String VERSION = \"\1 (${DATE})\"/g" src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java && \
+  ./bootstrap && \
+  ./configure --enable-server --enable-agent --with-mysql --enable-java \
+              --with-net-snmp --with-libcurl --with-libxml2 --with-openipmi \
+              --enable-ipv6 --with-jabber --with-openssl --with-ssh2 \
+              --with-ldap --with-unixodbc && \
+  make dbschema && \
+  gem install sass && \
+  make css && \
+  make install && \
+  mv /health/ /usr/local/src/zabbix/frontends/php/  && \
+  cp /usr/local/etc/web/zabbix.conf.php /usr/local/src/zabbix/frontends/php/conf/  && \
+  pip install py-zabbix && \
+  apt-get -y autoremove \
+             gcc build-essential pkg-config \
+             libopenipmi-dev libcurl4-openssl-dev libmysqld-dev \
+             openjdk-8-jdk ruby subversion gcc build-essential \
+             gettext python-setuptools swig python-imaging \
+             python-m2crypto python-iso8601
 
-
-# Layer: zabbix-vpoller
-RUN \
-    #git clone --no-checkout https://github.com/dnaeon/py-vpoller /usr/local/src/vpoller-module && \
-    #cd /usr/local/src/vpoller-module && \
-    #git config core.sparseCheckout true && \
-    #echo "extra/zabbix/vpoller-module*"> .git/info/sparse-checkout && \
-    #git checkout master && \
-    #mv extra/zabbix/vpoller-module/* . && \
-    #rm -Rf extra && \
-    #export ZABBIX_SF_VERSION="$(echo ${ZABBIX_VERSION} | cut -d/ -f2)" && \
-    #cd /usr/local/src/ && \
-    #tar xvfz zabbix-${ZABBIX_SF_VERSION}.tar.gz && \
-    #cd zabbix-${ZABBIX_SF_VERSION} && \
-    #./configure && \
-    #cp -a /usr/local/src/vpoller-module src/modules && \
-    #cd src/modules/vpoller-module && \
-    #make && \
-    #cp vpoller.so /usr/local/src/vpoller-module && \
-    #cd /usr/local/src/ && \
-    #export ZS_LoadModule=vpoller.so && \
-    export ZS_Include=/usr/local/etc/zabbix_server.conf.d/*.conf && \
-    rm -Rf zabbix-${ZABBIX_SF_VERSION} zabbix-${ZABBIX_SF_VERSION}.tar.gz
-
-
-CMD ["/config/vpoller-bootstrap.sh"]
+# Clean up APT when done.
+RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
---
+++ container-files-zabbix/config/init/bootstrap.sh
@@ -0,0 +1,275 @@
+#!/bin/sh
+
+set -eu
+export TERM=xterm
+# Bash Colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+white=`tput setaf 7`
+bold=`tput bold`
+reset=`tput sgr0`
+separator=$(echo && printf '=%.0s' {1..100} && echo)
+
+# Logging functions
+log() {
+  if [[ "$@" ]]; then echo "${bold}${green}[LOG `date +'%T'`]${reset} $@";
+  else echo; fi
+}
+warning() {
+  echo "${bold}${yellow}[WARNING `date +'%T'`]${reset} ${yellow}$@${reset}";
+}
+error() {
+  echo "${bold}${red}[ERROR `date +'%T'`]${reset} ${red}$@${reset}";
+}
+create_db() {
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -e "CREATE DATABASE IF NOT EXISTS ${ZS_DBName} CHARACTER SET utf8;"
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -e "GRANT ALL ON ${ZS_DBName}.* TO '${ZS_DBUser}'@'%' identified by '${ZS_DBPassword}';"
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -e "flush privileges;"
+}
+import_zabbix_db() {
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -D ${ZS_DBName} < ${ZABBIX_SQL_DIR}/schema.sql
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -D ${ZS_DBName} < ${ZABBIX_SQL_DIR}/images.sql
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -D ${ZS_DBName} < ${ZABBIX_SQL_DIR}/data.sql
+}
+logging() {
+  mkdir -p /var/log/zabbix
+  chmod 777 /var/log/zabbix
+  touch /var/log/zabbix/zabbix_server.log /var/log/zabbix/zabbix_agentd.log
+  chmod 777 /var/log/zabbix/zabbix_server.log /var/log/zabbix/zabbix_agentd.log
+}
+system_pids() {
+  touch /var/run/zabbix_server.pid /var/run/zabbix_agentd.pid /var/run/zabbix_java.pid
+  chmod 777 /var/run/zabbix_server.pid /var/run/zabbix_agentd.pid /var/run/zabbix_java.pid
+}
+fix_permissions() {
+  getent group zabbix || groupadd zabbix
+  getent passwd zabbix || useradd -g zabbix -M zabbix
+  chown -R $ZS_User:$ZS_User /usr/local/etc/
+  chown -R $ZS_User:$ZS_User /usr/local/src/zabbix/
+  mkdir -p /usr/local/src/zabbix/frontends/php/conf/
+  chmod 777 /usr/local/src/zabbix/frontends/php/conf/
+  chmod u+s /usr/bin/ping
+  chown root:$ZS_User /usr/sbin/fping
+  chown root:$ZS_User /usr/sbin/fping6
+  chmod 4710 /usr/sbin/fping
+  chmod 4710 /usr/sbin/fping6
+}
+update_config() {
+  # ^ZS_: /usr/local/etc/zabbix_server.conf
+  for i in $( set -o posix ; set | grep ^ZS_ | grep -v ^ZS_Include | grep -v ^ZS_LoadModule | grep -v ^ZS_SourceIP | grep -v ^ZS_TLS | sort -rn ); do
+    reg=$(echo ${i} | awk -F'=' '{print $1}')
+    val=$(echo ${i} | awk -F'=' '{print $2}')
+    sed -i "s#=${reg}\$#=${val}#g" /usr/local/etc/zabbix_server.conf
+    sed -i "s#${reg}#${val}#g" /usr/local/src/zabbix/frontends/php/conf/zabbix.conf.php
+  done
+  if [ "$ZS_SourceIP" != "" ]; then
+    sed -i -e "\|^SourceIP=|d" /usr/local/etc/zabbix_server.conf
+    echo SourceIP=${ZS_SourceIP} >> /usr/local/etc/zabbix_server.conf
+  fi
+  if [ "$ZS_Include" != "" ]; then
+    sed -i -e "\|^Include=|d" /usr/local/etc/zabbix_server.conf
+    echo Include=${ZS_Include} >> /usr/local/etc/zabbix_server.conf
+  fi
+  if [ "$ZS_LoadModule" != "" ]; then
+    sed -i -e "\|^LoadModule=|d" /usr/local/etc/zabbix_server.conf
+    echo LoadModule=${ZS_LoadModule} >> /usr/local/etc/zabbix_server.conf
+  fi
+  if [ "$ZS_TLSCAFile" != "" ]; then
+    sed -i -e "\|^TLSCAFile=|d" /usr/local/etc/zabbix_server.conf
+    echo TLSCAFile=${ZS_TLSCAFile} >> /usr/local/etc/zabbix_server.conf
+  fi
+  if [ "$ZS_TLSCRLFile" != "" ]; then
+    sed -i -e "\|^TLSCRLFile=|d" /usr/local/etc/zabbix_server.conf
+    echo TLSCRLFile=${ZS_TLSCRLFile} >> /usr/local/etc/zabbix_server.conf
+  fi
+  if [ "$ZS_TLSCertFile" != "" ]; then
+    sed -i -e "\|^TLSCertFile=|d" /usr/local/etc/zabbix_server.conf
+    echo TLSCertFile=${ZS_TLSCertFile} >> /usr/local/etc/zabbix_server.conf
+  fi
+  if [ "$ZS_TLSCAFile" != "" ]; then
+    sed -i -e "\|^TLSKeyFile=|d" /usr/local/etc/zabbix_server.conf
+    echo TLSKeyFile=${ZS_TLSKeyFile} >> /usr/local/etc/zabbix_server.conf
+  fi
+
+  # ^ZA_: /usr/local/etc/zabbix_agentd.conf
+  export ZA_Hostname_e=$(echo ${ZA_Hostname} | sed -e 's/ /\\\ /g')
+  sed -i "s#ZA_Hostname#${ZA_Hostname_e}#g" /usr/local/etc/zabbix_agentd.conf
+  unset ZA_Hostname_e
+  for i in $( set -o posix ; set | grep ^ZA_ | sort -rn ); do
+    reg=$(echo ${i} | awk -F'=' '{print $1}')
+    val=$(echo ${i} | awk -F'=' '{print $2}')
+    sed -i "s#=${reg}\$#=${val}#g" /usr/local/etc/zabbix_agentd.conf
+  done
+  if [ "$ZA_TLSCAFile" != "" ]; then
+    sed -i -e "\|^TLSCAFile=|d" /usr/local/etc/zabbix_agentd.conf
+    echo TLSCAFile=${ZA_TLSCAFile} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_TLSCRLFile" != "" ]; then
+    sed -i -e "\|^TLSCRLFile=|d" /usr/local/etc/zabbix_agentd.conf
+    echo TLSCRLFile=${ZA_TLSCRLFile} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_TLSServerCertIssuer" != "" ]; then
+    sed -i -e "\|^TLSServerCertIssuer=|d" /usr/local/etc/zabbix_agentd.conf
+    echo TLSServerCertIssuer=${ZA_TLSServerCertIssuer} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_TLSServerCertSubject" != "" ]; then
+    sed -i -e "\|^TLSServerCertSubject=|d" /usr/local/etc/zabbix_agentd.conf
+    echo TLSServerCertSubject=${ZA_TLSServerCertSubject} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_TLSCertFile" != "" ]; then
+    sed -i -e "\|^TLSCertFile=|d" /usr/local/etc/zabbix_agentd.conf
+    echo TLSCertFile=${ZA_TLSCertFile} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_TLSKeyFile" != "" ]; then
+    sed -i -e "\|^TLSKeyFile=|d" /usr/local/etc/zabbix_agentd.conf
+    echo TLSKeyFile=${ZA_TLSKeyFile} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_TLSPSKIdentity" != "" ]; then
+    sed -i -e "\|^TLSPSKIdentity=|d" /usr/local/etc/zabbix_agentd.conf
+    echo TLSPSKIdentity=${ZA_TLSPSKIdentity} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_TLSPSKFile" != "" ]; then
+    sed -i -e "\|^TLSPSKFile=|d" /usr/local/etc/zabbix_agentd.conf
+    echo TLSPSKFile=${ZA_TLSPSKFile} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_SourceIP" != "" ]; then
+    sed -i -e "\|^SourceIP=|d" /usr/local/etc/zabbix_agentd.conf
+    echo SourceIP=${ZA_SourceIP} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_LoadModulePath" != "" ]; then
+    sed -i -e "\|^LoadModulePath=|d" /usr/local/etc/zabbix_agentd.conf
+    echo LoadModulePath=${ZA_LoadModulePath} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_UserParameter" != "" ]; then
+    sed -i -e "\|^UserParameter=|d" /usr/local/etc/zabbix_agentd.conf
+    echo UserParameter=${ZA_UserParameter} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_HostMetadata" != "" ]; then
+    sed -i -e "\|^HostMetadata=|d" /usr/local/etc/zabbix_agentd.conf
+    echo HostMetadata=${ZA_HostMetadata} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_Include" != "" ]; then
+    sed -i -e "\|^Include=|d" /usr/local/etc/zabbix_agentd.conf
+    echo Include=${ZA_Include} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_Hostname" != "" ]; then
+    sed -i -e "\|^Hostname=|d" /usr/local/etc/zabbix_agentd.conf
+    echo Hostname=${ZA_Hostname} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+  if [ "$ZA_HostnameItem" != "" ]; then
+    sed -i -e "\|^HostnameItem=|d" /usr/local/etc/zabbix_agentd.conf
+    echo HostnameItem=${ZA_HostnameItem} >> /usr/local/etc/zabbix_agentd.conf
+  fi
+
+  # ^ZW_: /usr/local/src/zabbix/frontends/php/conf/zabbix.conf.php
+  export ZW_ZBX_SERVER_NAME_e=$(echo ${ZW_ZBX_SERVER_NAME} | sed -e 's/ /\\\ /g')
+  sed -i "s#ZW_ZBX_SERVER_NAME#${ZW_ZBX_SERVER_NAME_e}#g" /usr/local/src/zabbix/frontends/php/conf/zabbix.conf.php
+  unset ZW_ZBX_SERVER_NAME_e
+  for i in $( set -o posix ; set | grep ^ZW_ | grep -v ^ZW_ZBX_SERVER_NAME | sort -rn ); do
+    reg=$(echo ${i} | awk -F'=' '{print $1}')
+    val=$(echo ${i} | awk -F'=' '{print $2}')
+    sed -i "s#${reg}#${val}#g" /usr/local/src/zabbix/frontends/php/conf/zabbix.conf.php
+  done
+
+  # ^PHP_: /etc/php.d/zz-zabbix.ini
+  for i in $( set -o posix ; set | grep ^PHP_ | sort -rn ); do
+    reg=$(echo ${i} | awk -F'=' '{print $1}')
+    val=$(echo ${i} | awk -F'=' '{print $2}')
+    sed -i "s#${reg}\$#${val}#g" /etc/php.d/zz-zabbix.ini
+  done
+
+  if [ -f /etc/custom-config/php-zabbix.ini ]; then
+    cp -f /etc/custom-config/php-zabbix.ini /etc/php.d/zz-zabbix.ini
+  fi
+  if [ -f /etc/custom-config/zabbix_server.conf ]; then
+    cp -f /etc/custom-config/zabbix_server.conf /usr/local/etc/zabbix_server.conf
+  fi
+}
+####################### End of default settings #######################
+# Zabbix default sql files
+ZABBIX_SQL_DIR="/usr/local/src/zabbix/database/mysql"
+# load DB config from custom config file if exist
+if [ -f /etc/custom-config/zabbix_server.conf ]; then
+  FZS_DBPassword=$(grep ^DBPassword= /etc/custom-config/zabbix_server.conf | awk -F= '{print $2}')
+  if [ ! -z "$FZS_DBPassword" ]; then
+    export ZS_DBPassword=$FZS_DBPassword
+  fi
+  FZS_DBUser=$(grep ^DBUser= /etc/custom-config/zabbix_server.conf | awk -F= '{print $2}')
+  if [ ! -z "$FZS_DBUser" ]; then
+    export ZS_DBUser=$FZS_DBUser
+  fi
+  FZS_DBHost=$(grep ^DBHost= /etc/custom-config/zabbix_server.conf | awk -F= '{print $2}')
+  if [ ! -z "$FZS_DBHost" ]; then
+    export ZS_DBHost=$FZS_DBHost
+  fi
+  FZS_DBPort=$(grep ^DBPort= /etc/custom-config/zabbix_server.conf | awk -F= '{print $2}')
+  if [ ! -z "$FZS_DBPort" ]; then
+    export ZS_DBPort=$FZS_DBPort
+  fi
+  FZS_DBName=$(grep ^ZS_DBName= /etc/custom-config/zabbix_server.conf | awk -F= '{print $2}')
+  if [ ! -z "$FZS_DBName" ]; then
+    export ZS_DBName=$FZS_DBName
+  fi
+fi
+log "Preparing server configuration"
+update_config
+log "Config updated."
+log "Enabling logging and pid management"
+logging
+system_pids
+fix_permissions
+log "Done"
+
+if $ZS_enabled; then
+  # wait 120sec for DB server initialization
+  retry=24
+  log "Waiting for database server"
+  until mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -e "exit" &>/dev/null
+  do
+    log "Waiting for database server, it's still not available"
+    retry=`expr $retry - 1`
+    if [ $retry -eq 0 ]; then
+      error "Database server is not available!"
+      exit 1
+    fi
+    sleep 5
+  done
+  log "Database server is available"
+
+  log "Checking if database exists or SQL import is required"
+  if ! mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -e "use ${ZS_DBName};" &>/dev/null; then
+    warning "Zabbix database doesn't exist. Installing and importing default settings"
+    log `create_db`
+    log "Database and user created, importing default SQL"
+    log `import_zabbix_db`
+    log "Import finished, starting"
+  else
+    log "Zabbix database exists, starting server"
+  fi
+else
+  # Zabbix server is disabled
+  rm -rf /etc/supervisor.d/zabbix-server.conf
+fi
+
+if ! $ZA_enabled; then
+  # Zabbix agent is disabled
+  rm -rf /etc/supervisor.d/zabbix-agent.conf
+fi
+
+if ! $ZW_enabled; then
+  # Zabbix web UI is disabled
+  rm -rf /etc/supervisor.d/nginx.conf
+  rm -rf /etc/supervisor.d/php-fpm.conf
+fi
+
+if ! $SNMPTRAP_enabled; then
+  # SNMP trap process is disabled
+  rm -rf /etc/supervisor.d/snmptrapd.conf
+  rm -rf /etc/logrotate.d/zabbix-traps
+fi
+
+# Zabbix version detection
+export ZABBIX_VERSION=$(zabbix_server -V | grep Zabbix | awk '{print $3}')
+
+log "Starting Zabbix version $ZABBIX_VERSION"
